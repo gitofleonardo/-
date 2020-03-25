1f209ecdfd2ae6e6a74e55fc58329e6222d6c66a
leonardo <971840889@qq.com>
2020-03-25 19:05:50 +0800

    1

--- normal.js
+++ normal.js
@@ -1,12 +1,34 @@
-var idCardFrontAvailable=false
-var idCardBackAvailable=false
-var bankCardAvailable=false
+var idCardFrontAvailable=true
+var idCardBackAvailable=true
+var bankCardAvailable=true
 var contractList=[]
 var insList=[]
 
 $(document).ready(function(){
     initPaint()
+    loadCompany()
 })
+function loadCompany(){
+    $.ajax({
+        url:"http://localhost:82/qiye.php",
+        type:"GET",
+        crossDomain:true,
+        data:"",
+        success:function(result){
+            var coms=JSON.parse(result)
+            var container=document.getElementById("company")
+            for (var i=0;i<coms.length;i++){
+                var option=document.createElement("option")
+                option.value=coms[i]["id"]
+                option.innerText=coms[i]["name"]
+                container.appendChild(option)
+            }
+        },
+        error:function(){
+            alert("获取单位信息失败，请刷新页面")
+        }
+    })
+}
 function clearName(){
     $("#sign-name").jSignature("clear")
 }
@@ -27,7 +49,7 @@ function chooseBankCard(){
     console.log("click choose")
     $("#bank-card-file").click()
 }
-function onBankCardChange(){
+function onBankCardChanged(){
 
 }
 function onIdImageChangedFront(event){
@@ -52,12 +74,13 @@ function onIdImageChangedBack(event){
 }
 
 function checkIDCardFront(dom){
+    console.log(dom.files[0])
     var progress=document.getElementById("front-progress")
     var hint=document.getElementById("front-title")
     progress.style.display="block"
     progress.src="loading.gif"
     hint.innerText="正在验证"
-    var target="http://localhost/CheckFront"
+    var target="http://localhost:82/idcardnormal.php"
     var data=new FormData()
     data.append("front",dom.files[0])
     $.ajax({
@@ -160,8 +183,101 @@ function conformSubmit(){
     var bankName=$("#bank-name").value
     var applyAmount=$("#apply-amount").value
     var signNameInput=$("#sign-name-input").value
+    if (username=="" 
+    || idCode=="" 
+    || contactNumber==""
+    || companyId==-1
+    || userPost==""
+    || entryTime==""
+    || canbaoTime==""
+    || originCompany==""
+    || originCompanyLocation==""
+    || bankCardNumber==""
+    || bankName==""
+    || applyAmount==""){
+        alert("信息未完整！请仔细检查")
+        return
+    }
     //文件数据
+    var idFront=document.getElementById("id-card-img-front").files[0]
+    var idBack=document.getElementById("id-card-img-back").files[0]
+    var bankCardImg=document.getElementById("bank-card-file").files[0]
+    var laborContracts=document.getElementById("add-labor-contract")
+    var laborContractCount=laborContracts.children.length
+    if (laborContractCount<=2){
+        alert("您还未上传劳动合同")
+        return
+    }
+    var insRecords=document.getElementById("add-ins-entry")
+    var insRecordCount=insRecords.children.length
+    if (insRecordCount<=2){
+        alert("您还未上传养老保险明细记录")
+        return
+    }
+    var contracts=[]
+    var start=0
+    var i=2
+    for (;i<laborContractCount;i++){
+        contracts.push(laborContracts.children[i].withFile)
+        start++
+    }
+    start=0
+    var ins=[]
+    i=2
+    for (;i<insRecordCount;i++){
+        ins.push(insRecords.children[i].withFile)
+        start++
+    }
+    console.log(contracts)
+    console.log(ins)
+    var signature=$("#sign-name").jSignature("getData","default")
+    console.log($("#sign-name").jSignature("getData","native"))
+    console.log(signNameInput)
+    if ($("#sign-name").jSignature("getData","native").length==0 && (signNameInput==undefined || signNameInput=="")){
+        alert("手写签名和输入框签名必须至少填写一项")
+        return
+    }
+    var formData=new FormData()
+    formData.append("username",username)
+    formData.append("idCode",idCode)
+    formData.append("phone",contactNumber)
+    formData.append("company",companyId)
+    formData.append("userPost",userPost)
+    formData.append("entryTime",entryTime)
+    formData.append("canbaoTime",canbaoTime)
+    formData.append("originCompany",originCompany)
+    formData.append("originCompanyLocation",originCompanyLocation)
+    formData.append("bankCardNumber",bankCardNumber)
+    formData.append("bankName",bankName)
+    formData.append("applyAmount",applyAmount)
+    formData.append("signNameInput",signNameInput)
+    formData.append("idCardImgFront",idFront)
+    formData.append("idCardImgBack",idBack)
+    formData.append("bankCardImg",bankCardImg)
+    formData.append("signature",signature)
     
+    var i=0
+    for (;i<contracts.length;i++){
+        formData.append("contracts",contracts[i])
+    }
+    i=0
+    for (;i<ins.length;i++){
+        formData.append("ins",ins[i])
+    }
+    $.ajax({
+        url:"http://localhost/PostData",
+        type:"post",
+        mimeType: "multipart/form-data",
+        processData:false,
+        data:formData,
+        contentType:false,
+        success:function(result){
+            console.log(result)
+        },
+        error:function(){
+
+        }
+    })
 }
 function onAddContract(event){
     var container=document.getElementById("add-labor-contract")
@@ -191,7 +307,6 @@ function onAddContract(event){
     container.appendChild(imgContainer)
 }
 function onAddInsChanged(event){
-    console.log($("#add-ins-input").value)
     var container=document.getElementById("add-ins-entry")
     var imgFile=event.target.files[0]
     contractList.push(imgFile)
